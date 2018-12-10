5756c574dc70570d7d4f85bf3dccc1d371f7b84b
Vladislav Sovrasov <sovrasov.vlad@gmail.com>
2018-12-10 16:17:08 +0300

    Optimize computation of buonds

--- transportation_solver.py
+++ transportation_solver.py
@@ -5,11 +5,12 @@ import abc
 FLT_INF = float('inf')
 
 class node:
-    def __init__(self, v=[], upper=FLT_INF, lower=FLT_INF, partial_obective=FLT_INF):
+    def __init__(self, v=[], upper=FLT_INF, lower=FLT_INF, partial_objective=FLT_INF, partial_time=0):
         self.v = v
         self.upper = upper
         self.lower = lower
-        self.partial_objective = partial_obective
+        self.partial_objective = partial_objective
+        self.partial_time = partial_time
 
     def __lt__(self, other):
         return self.v < other.v
@@ -102,47 +103,68 @@ def compute_objective(v, problem):
     assert len(v) == problem['n']
     return compute_partial_objective(v, problem)[1]
 
-def compute_lower_bound_parallel(v, problem):
-    if len(v) == problem['n']:
-        return compute_objective(v, problem)
+def update_partial_opjective(vertex, problem):
+    assert vertex.v
 
-    time, objective = compute_partial_objective(v, problem)
-    not_visited = [i for i in range(1, problem['n'] + 1) if not i in v]
+    t = len(vertex.v) - 1
+    if t == 0:
+        time = problem['delays_matrix'][0][vertex.v[0]]
+        objective = 0
+        if time > problem['limits'][vertex.v[0] - 1]:
+            objective += 1
+        vertex.partial_objective = objective
+        vertex.partial_time = time
+    else:
+        vertex.partial_time += problem['delays_matrix'][vertex.v[t - 1]][vertex.v[t]]
+        if vertex.partial_time > problem['limits'][vertex.v[t] - 1]:
+            vertex.partial_objective += 1
+
+    return vertex
+
+def compute_lower_bound_parallel(vertex, problem):
+    if len(vertex.v) == problem['n']:
+        return vertex.partial_obective
+
+    not_visited = list(set(range(1, problem['n'] + 1)) - set(vertex.v))
+    time = vertex.partial_time
+    objective = vertex.partial_objective
+
     for i in not_visited:
-        if time + problem['delays_matrix'][v[-1]][i] > problem['limits'][i - 1]:
+        if time + problem['delays_matrix'][vertex.v[-1]][i] > problem['limits'][i - 1]:
             objective += 1
 
     return objective
 
-def compute_upper_bound_greedy(v, problem):
-    if len(v) == problem['n']:
-        return compute_objective(v, problem)
+def compute_upper_bound_greedy(vertex, problem):
+    if len(vertex.v) == problem['n']:
+        return vertex.partial_obective
 
-    v_ = list(v)
+    greedy_v = node(list(vertex.v), partial_objective=vertex.partial_objective, partial_time=vertex.partial_time)
 
-    while len(v_) < problem['n']:
-        not_visited = [i for i in range(1, problem['n'] + 1) if not i in v_]
+    while len(greedy_v.v) < problem['n']:
+        not_visited = list(set(range(1, problem['n'] + 1)) - set(greedy_v.v))
         w = []
-        z, _ = compute_partial_objective(v_, problem)
+        z = greedy_v.partial_time
         for beta in not_visited:
             t_d = problem['limits'][beta - 1]
-            t = z + problem['delays_matrix'][v_[-1]][beta]
+            t = z + problem['delays_matrix'][greedy_v.v[-1]][beta]
             if t > t_d:
                 w.append(float('inf'))
             else:
                 w.append(t_d - t)
         best_idx = w.index(min(w))
-        v_.append(not_visited[best_idx])
+        greedy_v.v.append(not_visited[best_idx])
+        greedy_v = update_partial_opjective(greedy_v, problem)
 
-    return v_, compute_objective(v_, problem)
+    return greedy_v.v, greedy_v.partial_objective
 
-def branch(v, n):
-    assert len(v) <= n
+def branch(vertex, n):
+    assert len(vertex.v) < n
     new_nodes = []
 
-    for i in range(1, n + 1):
-        if i not in v:
-            new_nodes.append(v + [i])
+    not_visited = list(set(range(1, n + 1)) - set(vertex.v))
+    for i in not_visited:
+        new_nodes.append(node(vertex.v + [i], vertex.upper, vertex.lower, vertex.partial_objective, vertex.partial_time))
 
     return new_nodes
 
@@ -175,18 +197,19 @@ def solve_transportation(problem, branch_strategy='breadth-first',
             if vertex.lower < best_point.lower:
                 best_point = vertex
         else:
-            new_v = branch(vertex.v, n)
-            for v in new_v:
-                lower = compute_lower_bound(v, problem)
-                if len(v) < n:
-                    v_, upper = compute_upper_bound(v, problem)
-                    assert len(v_) == n
-                    if upper < best_point.lower:
-                        best_point = node(v_, upper, upper)
+            new_nodes = branch(vertex, n)
+            for new_vertex in new_nodes:
+                new_vertex = update_partial_opjective(new_vertex, problem)
+                new_vertex.lower = compute_lower_bound(new_vertex, problem)
+                if len(new_vertex.v) < n:
+                    greedy_solution, new_vertex.upper = compute_upper_bound(new_vertex, problem)
+                    assert len(greedy_solution) == n
+                    if new_vertex.upper < best_point.lower:
+                        best_point = node(greedy_solution, new_vertex.upper, new_vertex.upper)
                 else:
-                    upper = lower
-                best_upper_bound = min(best_upper_bound, upper)
-                if not (lower >= best_upper_bound):
-                    nodes.put(node(v, lower, upper))
+                    new_vertex.upper = new_vertex.lower
+                best_upper_bound = min(best_upper_bound, new_vertex.upper)
+                if not (new_vertex.lower >= best_upper_bound):
+                    nodes.put(new_vertex)
 
     return best_point.lower, best_point.v, iters
